017e58d2cdcfaf9e2b0a61535bbc11cd82cec8e6
Gabriel Guimarães <gabrielguimaraes.dev@gmail.com>
2023-11-13 13:04:51 -0300

    REQ 4e5

--- projects/serializers.py
+++ projects/serializers.py
@@ -1,5 +1,10 @@
 from rest_framework import serializers
-from projects.models import Profile, Project
+from projects.models import (
+    Profile,
+    Project,
+    Certificate,
+    CertifyingInstitution,
+)
 
 
 class ProfileSerializer(serializers.ModelSerializer):
@@ -12,3 +17,33 @@ class ProjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = Project
         fields = "__all__"
+
+
+class NoCretifiedInstitutionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Certificate
+        fields = ["id", "name"]
+
+
+class CertifiedInstitutionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Certificate
+        fields = "__all__"
+
+
+class CertifyingInstitutionSerializer(serializers.ModelSerializer):
+    certificates = NoCretifiedInstitutionSerializer(many=True)
+
+    class Meta:
+        model = CertifyingInstitution
+        fields = ["id", "name", "url", "certificates"]
+
+    def create(self, validated_data):
+        certificates_data = validated_data.pop("certificates")
+        new = CertifyingInstitution.objects.create(**validated_data)
+        for certificate_data in certificates_data:
+            Certificate.objects.create(
+                certifying_institution=new,
+                **certificate_data,
+            )
+        return new
--- projects/views.py
+++ projects/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from rest_framework import viewsets
-from .models import Profile, Project, permissions
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from .models import Profile, Project, Certificate, CertifyingInstitution
 from .serializers import ProfileSerializer, ProjectSerializer
+from .serializers import CertifiedInstitutionSerializer as certified
+from .serializers import CertifyingInstitutionSerializer as certifying
 
 
 class ProfileViewSet(viewsets.ModelViewSet):
@@ -10,22 +13,32 @@ class ProfileViewSet(viewsets.ModelViewSet):
 
     def get_permissions(self):
         if self.request.method == "GET":
-            return [permissions.AllowAny()]
-        return [permissions.IsAuthenticated()]
+            return [AllowAny()]
+        return [IsAuthenticated()]
 
     def retrieve(self, request, *args, **kwargs):
         if request.method == "GET":
-            id = kwargs.get("pk")
-            profile = Profile.objects.get(pk=id)
-
-            return render(
-                request,
-                "profile_detail.html",
-                {"profile": profile},
-            )
+            profile_id = kwargs.get("pk")
+            profile = Profile.objects.get(pk=profile_id)
+            projects = profile.projects.all()
+            context = {
+                "profile": profile,
+                "projects": projects,
+            }
+            return render(request, "profile_detail.html", context)
         return super().retrieve(request, *args, **kwargs)
 
 
 class ProjectViewSet(viewsets.ModelViewSet):
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
+
+
+class CertificateViewSet(viewsets.ModelViewSet):
+    queryset = Certificate.objects.all()
+    serializer_class = certified
+
+
+class CertifyingInstitutionViewSet(viewsets.ModelViewSet):
+    queryset = CertifyingInstitution.objects.all()
+    serializer_class = certifying
